0dddc9e418f4d390ca71cc2ce5797619c2954bbd
Luffy18346 <akashkoradia1@gmail.com>
2019-02-05 13:39:13 +0530

    Header Component Started

---
+++ app/component/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+const Header = () => {
+    return (
+        <Text>
+            asjsda jao jdaosjdohhxjjd;vmb mkfxl dkbf jdlkmg nbjrielkfvn jieaLkn bjewkal>KjhgduyiowuaJv
+        </Text>
+    )
+}
+
+export default Header;
